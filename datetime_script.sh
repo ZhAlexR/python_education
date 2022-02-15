88a4a5cf22ec7c28dc793e0e7db656fb51d71e6d
Zhukov_Oleksandr <zhukov9523@gmail.com>
2022-02-15 11:09:06 +0200

    Initial commit
    Added: datesime_script.sh
    
    Empty get_weather.sh
    
    0 */1 * * * /home/zhukov/programming/python_education/datetime_script.sh
    
    Every hour script
    
    corontab
    
    	0 7 * * * /home/zhukov/programming/python_education/get_weather.sh
    
    Script worksgit add datetime.txt !
    
    Added path to json file

---
+++ datetime_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+date_time=$(date +'%d/%m/%y - %T')
+echo "$date_time" >> /home/zhukov/programming/python_education/datetime.txt
---
+++ get_weather.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -o /home/zhukov/programming/python_education/weather.json  http://api.openweathermap.org/data/2.5/weather?lat=49.9923181&lon=36.2310146&appid=c827718590fb4f19c9caba715785c141
